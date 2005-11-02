312d9035bb0c1c63d0ff71bf10892b3ae94b1519
David Grant <djg@php.net>
2005-11-02 21:11:20 +0000

    Changed license notice from PHP license to GNU GPL.  This will be in effect from the next release.
    git-svn-id: http://svn.php.net/repository/pear/packages/File_Ogg/trunk@199868 c90b9560-bf6c-de11-be94-00142212c4b1

--- File/Ogg/Speex.php
+++ File/Ogg/Speex.php
@@ -1,22 +1,26 @@
 <?php
-// +----------------------------------------------------------------------+
-// | PHP version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at                              |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Author: David Grant <david@grant.org.uk>                  |
-// +----------------------------------------------------------------------+
+// +--------------------------------------------------------------------------------+
+// | File_Ogg PEAR Package for Accessing Ogg Bitstreams                             |
+// | Copyright (c) 2005 David Grant <david@grant.org.uk>                            |
+// +--------------------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or                  |
+// | modify it under the terms of the GNU General Public License                    |
+// | as published by the Free Software Foundation; either version 2                 |
+// | of the License, or (at your option) any later version.                         |
+// |                                                                                |
+// | This program is distributed in the hope that it will be useful,                |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of                 |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  |
+// | GNU General Public License for more details.                                   |
+// |                                                                                |
+// | You should have received a copy of the GNU General Public License              |
+// | along with this program; if not, write to the Free Software                    |
+// | Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.|
+// +--------------------------------------------------------------------------------+
 
 /**
  * @author 		David Grant <david@grant.org.uk>
+ * @copyright 	David Grant <david@grant.org.uk>
  * @category	File
  * @package 	File_Ogg
  * @version 	CVS: $Id$
